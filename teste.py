6ba592798e390b75341177a3f54349cff0209680
Rafael <franciscoandersonrafael@gmail.com>
2022-05-15 16:47:01 -0300

    mini projetos em python

--- teste.py
+++ teste.py
@@ -1,6 +1,26 @@
+#calculando a media de notas
+
+#n1 =int (input("nota 01:"))
+#n2 =int (input("nota 02:"))
+#n3 =int (input("nota 03:"))
+#m = (n1+n2+n3)/3
+
+#print(' A nota 1 é:{0}\n a nota 2 é:{1}\n a nota 3 é:{2} a media é:{3}'.format(n1,n2,n3,m))
 
 
-n1 = int (input ("digite um numero"))
-n2 = int (input ("digite outro numero"))
-S = n1+n2
-print('A soma entre {0} e {01} vale {02}' .format(n1, n2, S))
+
+#calculando o dobro de um numero e sua raiz quadrada
+
+#n1 = int(input('digite um mumero:'))
+#d = n1+n1
+#r = n1*n1
+
+#print('numero: {} seu dobro: {} sua raiz quadrada : {}'.format(n1,d,r))
+
+#converter metros em centimentros e mililetros
+
+m = float (input('digite uma valor:'))
+cm = m*100
+mm = m*1000
+
+print('{} Metros\n{}cm convertido em cm\n{}mm convertido em mm'.format(m,cm,mm))
