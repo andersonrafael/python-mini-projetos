46c34e12c4fba89bbdbea01e5c3fd636ca8e2ac0
Rafael <franciscoandersonrafael@gmail.com>
2022-05-20 20:47:02 -0300

    mine projetos em python

--- teste.py
+++ teste.py
@@ -1,4 +1,4 @@
-#calculando a media de notas
+#>>>>>  CALCULANDO A MEDIA DE NOTAS  <<<<<
 
 #n1 =int (input("nota 01:"))
 #n2 =int (input("nota 02:"))
@@ -9,18 +9,43 @@
 
 
 
-#calculando o dobro de um numero e sua raiz quadrada
+#>>>>>  CALCULANDO O DOBRO DE UM NUMERO E SUA RAIZ QUADRADA  <<<<<<
 
-#n1 = int(input('digite um mumero:'))
-#d = n1+n1
-#r = n1*n1
+n1 = int(input('digite um mumero:'))
+d = n1+n1
+r = n1*n1
 
-#print('numero: {} seu dobro: {} sua raiz quadrada : {}'.format(n1,d,r))
+print('numero: {} seu dobro: {} sua raiz quadrada : {}'.format(n1,d,r))
 
-#converter metros em centimentros e mililetros
+#>>>>>>> CONVERTER METROS E CENTIMENTROS EM MILIMETROS <<<<<<
 
-m = float (input('digite uma valor:'))
-cm = m*100
-mm = m*1000
+#m = float (input('digite uma valor:'))
+#cm = m*100
+#mm = m*1000
 
-print('{} Metros\n{}cm convertido em cm\n{}mm convertido em mm'.format(m,cm,mm))
+#print('{} Metros\n{}cm convertido em cm\n{}mm convertido em mm'.format(m,cm,mm))
+ 
+#>>>>>>> CONVERSOR DE REAL PARA DOLAR <<<<<
+
+#real = int(input('quanto vc tem?'))
+#dolar = 4,98
+#con = (real * 4,98)
+
+#print('Você tem: {0} R$ \n O valor do dolar atualmente esta: {1} Dolares \n Voce pode comprar: {2} de #dolares'.format(real,dolar,con))
+
+#>>>>>>> CALCULADORA DE TINTA <<<<<<<<
+
+#A = int(input('Quantos metros de altura tem a parede?'))
+#L = int(input('Quantos metros de Largura tem a parede?'))
+#M = A*L
+#LT = M/2
+
+#print(' A area total é : {} metros quadrados \n Sera necessario {} Litros de tinta'.format(M,LT))
+
+#>>>>> CALCULANDO O VALOR DE UM PRODUTO COMM DESCONTO <<<<<
+
+#v = int(input('O valor do produto é: '))
+#vd = int(input("O valor do desconto é: "))
+#vf = v - vd
+
+#print('O valor do produto sem desconto é: {0}\n O valor dodesconto é: {1}\n O valor final do produto com desconto é: {2}'.format(v,vd,vf))
